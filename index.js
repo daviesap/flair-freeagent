1e1a98b33e233a3acef8c503972768b51d7d6f9f
Andrew Davies <andrew@flair.london>
2025-04-23 11:01:52 +0100

    Fix: use Functions Framework to bind to port 8080 Added pants

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
  * Responds to an HTTP request.
  */
 exports.getSecretTest = (req, res) => {
-  res.send("Hello from GitHub-deployed Gen 2 function!");
+  res.send("Hello from GitHub-deployed Gen 2 function! PANTS");
 };
