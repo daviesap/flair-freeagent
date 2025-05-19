68343c9128dff901f822e7dc0aa50e89c672b255
daviesap <77567470+daviesap@users.noreply.github.com>
2025-05-19 17:34:35 +0200

    Minor change - removed a commented line

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
 process.env.GOOGLE_CLOUD_PROJECT = 'flair-freeagent';
 
 import { authCallback } from './handlers/auth.js';
-//import { adminDashboard } from './handlers/dashboard.js';
 import { freeAgentHandler } from './handlers/freeagent.js';
 
 export { authCallback, freeAgentHandler as FreeAgent };
